b6888107dcb25471874a669d2c6570977e8fcc9e
Sajib Adhikary <60180521+sajibAdhi@users.noreply.github.com>
2024-06-11 06:06:10 +0000

    Update feed script path

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /usr/bin/feed.py
+python3 /app/feed.py
 
 git add -A && git commit -m "Update feed"
 git push --set-upstream origin main
